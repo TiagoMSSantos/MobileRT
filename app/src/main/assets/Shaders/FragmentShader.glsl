02fe8ed2b1f5aa3ee16661ab98c6dddc736d0ae8
TiagoMSSantos <tiago_s@outlook.pt>
2020-06-06 08:51:11 +0100

    GLSL: Add comments for readability in the GLSL shaders

--- app/src/main/assets/Shaders/FragmentShader.glsl
+++ app/src/main/assets/Shaders/FragmentShader.glsl
@@ -1,16 +1,28 @@
+// Fragment Shader: for the 2 triangles with the texture where the
+// Ray Tracing engine writes the bitmap
+
+// OpenGL ES 2.0 version
 #version 100
 
+// High precision is not necessary
 precision lowp float;
 
+// Texture parameter
 uniform sampler2D uniformTexture;
 
+// Interpolated variable from the vertex shader
 varying vec2 fragmentTexCoord;
 
+// Function for the tonemap
 float toneMap (const float value) {
 	return value;
 }
 
+// Main to calculate the color for each pixel in gl_FragColor
 void main () {
+	// Get the color for this pixel (interpolated from the nearest vertices)
 	vec4 color = texture2D(uniformTexture, fragmentTexCoord);
+
+	// Output the color for this pixel
 	gl_FragColor = vec4(toneMap(color[0]), toneMap(color[1]), toneMap(color[2]), 1.0);
 }
--- app/src/main/assets/Shaders/VertexShaderRaster.glsl
+++ app/src/main/assets/Shaders/VertexShaderRaster.glsl
@@ -1,17 +1,34 @@
+// Vertex Shader: for the rasterized scene (preview feature)
+
+// OpenGL ES 2.0 version
 #version 100
 
+// High precision is not necessary
 precision lowp float;
 
+// Model parameter
 uniform mat4 uniformModelMatrix;
+
+// View parameter
 uniform mat4 uniformViewMatrix;
+
+// Projection parameter
 uniform mat4 uniformProjectionMatrix;
 
+// Vertex position for each vertex
 attribute vec4 vertexPosition;
+
+// Vertex color for each vertex
 attribute vec4 vertexColor;
 
+// Interpolated variable to the fragment shader
 varying vec4 fragmentColor;
 
+// Main to calculate the position and color for each vertex in gl_Position and
+// fragmentColor
 void main () {
+    // Output the position for this vertex
 	gl_Position = uniformProjectionMatrix * uniformViewMatrix * uniformModelMatrix * vertexPosition;
+    // Output the color for this vertex
     fragmentColor = vertexColor;
 }
