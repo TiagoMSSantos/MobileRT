b7a450a51c017038047ecbc18c5f9a1f63b9d283
TiagoMSSantos <tiago_s@outlook.pt>
2020-10-28 08:38:07 +0000

    CI: Add conan support in CI
    Create install_dependencies script, which should install all necessary
    depencies for all Operating Systems.
    Also, add Windows build in Travis CI.
    Upgrade boost and googletest.

--- scripts/compile_native.sh
+++ scripts/compile_native.sh
@@ -24,6 +24,97 @@ source scripts/helper_functions.sh
 ###############################################################################
 
 ###############################################################################
+# Get the proper C compiler for conan
+# Possible values for clang are ['3.3', '3.4', '3.5', '3.6', '3.7', '3.8', '3.9', '4.0',
+# '5.0', '6.0', '7.0', '7.1', '8', '9', '10', '11']
+# Possible values for gcc (Apple clang) are ['4.1', '4.4', '4.5', '4.6', '4.7', '4.8',
+# '4.9', '5', '5.1', '5.2', '5.3', '5.4', '5.5', '6', '6.1', '6.2', '6.3', '6.4',
+# '6.5', '7', '7.1', '7.2', '7.3', '7.4', '7.5', '8', '8.1', '8.2', '8.3', '8.4',
+# '9', '9.1', '9.2', '9.3', '10', '10.1']
+###############################################################################
+if [[ "${compiler}" == *"clang++"* ]]; then
+  conan_compiler="clang"
+  conan_compiler_version=$(clang++ -dumpversion)
+  callCommand clang++ -v;
+  export CC=clang
+elif [[ "${compiler}" == *"g++"* ]]; then
+  conan_compiler="gcc"
+  conan_compiler_version=$(g++ -dumpversion)
+  callCommand g++ -v;
+  export CC=gcc
+fi
+export CXX="${compiler}";
+# Fix compiler version used
+if [ "${conan_compiler_version}" == "9.0.0" ]; then
+  conan_compiler_version=9
+fi
+if [ "${conan_compiler_version}" == "12.0.0" ]; then
+  conan_compiler_version=12
+fi
+###############################################################################
+###############################################################################
+
+###############################################################################
+# Get Conan path
+###############################################################################
+CONAN_PATH=$(find ~/ -name "conan" || true);
+echo "Conan binary: ${CONAN_PATH}"
+echo "Conan location: ${CONAN_PATH%/conan}"
+PATH=${CONAN_PATH%/conan}:${PATH}
+echo "PATH: ${PATH}"
+###############################################################################
+###############################################################################
+
+###############################################################################
+# Get CPU Architecture
+###############################################################################
+CPU_ARCHITECTURE=x86_64
+if [ -x "$(command -v uname)" ]; then
+  CPU_ARCHITECTURE=$(uname -m)
+  if [ "${CPU_ARCHITECTURE}" == "aarch64" ]; then
+    CPU_ARCHITECTURE=armv8
+  fi
+fi
+###############################################################################
+###############################################################################
+
+###############################################################################
+# Add Conan remote dependencies
+###############################################################################
+if [ -x "$(command -v conan)" ]; then
+  conan profile new default;
+  callCommand conan profile update settings.compiler="${conan_compiler}" default;
+  callCommand conan profile update settings.compiler.version="${conan_compiler_version}" default;
+  callCommand conan profile update settings.compiler.libcxx="libstdc++11" default;
+  callCommand conan profile update settings.arch="${CPU_ARCHITECTURE}" default;
+  callCommand conan profile update settings.os="Linux" default;
+  callCommand conan profile update settings.build_type="Release" default;
+  conan remote add bintray https://api.bintray.com/conan/bincrafters/public-conan;
+fi
+
+# Install C++ Conan dependencies
+function install_conan_dependencies() {
+#  ln -s configure/config.guess /home/travis/.conan/data/libuuid/1.0.3/_/_/build/b818fa1fc0d3879f99937e93c6227da2690810fe/configure/config.guess
+#  ln -s configure/config.sub /home/travis/.conan/data/libuuid/1.0.3/_/_/build/b818fa1fc0d3879f99937e93c6227da2690810fe/configure/config.sub
+
+  callCommand conan install \
+  -s compiler=${conan_compiler} \
+  -s compiler.version="${conan_compiler_version}" \
+  -s compiler.libcxx=libstdc++11 \
+  -s arch="${CPU_ARCHITECTURE}" \
+  -s os="Linux" \
+  -s build_type=Release \
+  -o bzip2:shared=True \
+  --build missing \
+  --profile default \
+  ../app/
+
+  export CONAN="TRUE"
+}
+###############################################################################
+###############################################################################
+
+###############################################################################
 # Compile for native
 ###############################################################################
 
@@ -41,9 +132,13 @@ fi
 
 function build() {
   callCommand cd "${build_path}"
+#  install_conan_dependencies
+
   echo "Calling CMake"
   callCommand cmake -DCMAKE_VERBOSE_MAKEFILE=ON \
-    -DCMAKE_CXX_COMPILER="${compiler}" -DCMAKE_BUILD_TYPE="${type}" ../app/ \
+    -DCMAKE_CXX_COMPILER="${compiler}" \
+    -DCMAKE_BUILD_TYPE="${type}" \
+    ../app/ \
     2>&1 | tee ./log_cmake_"${type}".log
   resCompile=${PIPESTATUS[0]}
 
---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+###############################################################################
+# Change directory to MobileRT root
+###############################################################################
+cd "$(dirname "${BASH_SOURCE[0]}")/.." || exit
+###############################################################################
+###############################################################################
+
+###############################################################################
+# Get helper functions
+###############################################################################
+source scripts/helper_functions.sh
+###############################################################################
+###############################################################################
+
+###############################################################################
+# Install dependencies
+###############################################################################
+# if Debian based Linux
+if [ -x "$(command -v apt-get)" ]; then
+  callCommand sudo apt-get update -y;
+  callCommand sudo apt-get install --no-install-recommends -y \
+    xorg-dev \
+    libxcb-render-util0-dev \
+    libxcb-xkb-dev \
+    libxcb-icccm4-dev \
+    libxcb-image0-dev \
+    libxcb-keysyms1-dev \
+    libxcb-xinerama0-dev \
+    libx11-xcb-dev \
+    libxcb-randr0-dev \
+    libxcb-shape0-dev \
+    libxcb-sync-dev \
+    libxcb-xfixes0-dev \
+    pkg-config \
+    bzip2 \
+    sqlite3 \
+    vim \
+    findutils \
+    make \
+    bash \
+    ca-certificates \
+    git \
+    libatomic1 \
+    qt5-default \
+    g++ \
+    build-essential \
+    lcov \
+    python3 \
+    python3-pip \
+    python3-dev \
+    python3-setuptools \
+    sudo \
+    || true;
+# if MacOS
+elif [ -x "$(command -v brew)" ]; then
+  callCommand brew update;
+  callCommand brew tap cartr/qt4;
+  callCommand brew uninstall --force openssl@1.0;
+  callCommand brew install libomp;
+  callCommand brew install openssl@1.0;
+  callCommand brew install qt@4;
+  callCommand brew install qt;
+  callCommand brew install llvm;
+  callCommand brew install python3;
+  callCommand brew install lcov;
+  callCommand brew install pyenv;
+fi
+###############################################################################
+###############################################################################
+
+###############################################################################
+# Install Conan package manager
+###############################################################################
+if [ -x "$(command -v choco)" ]; then
+  callCommand choco install python --version 3.8.0;
+fi
+callCommand python3 -m pip install --upgrade pip;
+callCommand pip3 install --upgrade setuptools pip;
+callCommand pip3 install scikit-build;
+callCommand pip3 install cmake --upgrade;
+callCommand pip3 install conan;
+#callCommand pip3 install clang;
+export PATH
+PATH=$(pip3 list -v | grep -i cmake | tr -s ' ' | cut -d ' ' -f 3):${PATH}
+PATH=$(pip3 list -v | grep -i conan | tr -s ' ' | cut -d ' ' -f 3):${PATH}
+
+CONAN_PATH=$(find ~/ -name "conan");
+echo "Conan binary: ${CONAN_PATH}"
+echo "Conan location: ${CONAN_PATH%/conan}"
+PATH=${CONAN_PATH%/conan}:${PATH}
+callCommand conan -v
+
+#CLANG_PATH=$(find / -name "clang");
+#echo "Clang binary: ${CLANG_PATH}"
+#echo "Clang location: ${CLANG_PATH%/clang}"
+#PATH=${CLANG_PATH%/clang}:${PATH}
+
+echo "PATH: ${PATH}"
+#/home/runner/.local/bin/conan
+###############################################################################
+###############################################################################
+
+###############################################################################
+# Test dependencies
+###############################################################################
+checkCommand vim
+checkCommand cmake
+checkCommand make
+checkCommand bash
+checkCommand git
+checkCommand g++
+#checkCommand clang++
+checkCommand python3
+checkCommand pip
+checkCommand pip3
+checkCommand conan
+###############################################################################
+###############################################################################
