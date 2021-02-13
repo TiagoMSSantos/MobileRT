ef5d3f0f9da929b1adbac83cca23187d314d7678
TiagoMSSantos <tiago_s@outlook.pt>
2021-02-13 10:27:59 +0000

    Android tests: Add small description for the Android tests

--- app/src/androidTest/java/puscas/mobilertapp/package-info.java
+++ app/src/androidTest/java/puscas/mobilertapp/package-info.java
@@ -1,4 +1,8 @@
 /**
  * The package of Android instrumentation tests.
+ *
+ * These tests run on real or virtual devices.
+ * Such tests include: integration tests, end-to-end tests, and other tests
+ * where the JVM alone cannot validate the application's functionality.
  */
 package puscas.mobilertapp;
--- app/src/test/java/puscas/mobilertapp/package-info.java
+++ app/src/test/java/puscas/mobilertapp/package-info.java
@@ -1,4 +1,6 @@
 /**
  * The package of Android unit tests.
+ *
+ * These are tests that run on the local machine, without the need for an Android device.
  */
 package puscas.mobilertapp;
