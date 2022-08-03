42f71e579f1eb67ba9a92496cf6ed73e92f76e36
TiagoMSSantos <tiago_s@outlook.pt>
2022-08-03 20:15:20 +0100

    build(scripts): Fix install of glibc on Arch based Linux
    Summary:
    This commit fixes the installation of glibc version on Arch based
    Linux.
    
    Test plan:
    * Trigger pipeline

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -122,12 +122,9 @@ function install_dependencies_red_hat() {
 }
 
 function install_dependencies_arch() {
-  patched_glibc=glibc-linux4-2.33-4-x86_64.pkg.tar.zst && \
-  curl -LO "https://repo.archlinuxcn.org/x86_64/${patched_glibc}" && \
-  bsdtar -C / -xvf "${patched_glibc}";
-
   pacman -Syu --noconfirm --needed;
   pacman -Sy --noconfirm --needed \
+    glibc lib32-glibc \
     vim \
     findutils \
     cmake make \
