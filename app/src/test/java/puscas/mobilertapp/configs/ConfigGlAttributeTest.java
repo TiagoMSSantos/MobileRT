86be93ddcdfb78c65761ce0a0e33022181ae5c50
TiagoMSSantos <tiago_s@outlook.pt>
2023-12-03 18:21:21 +0000

    test(ConfigGlAttributeTest): Add more unit tests

--- app/src/test/java/puscas/mobilertapp/configs/ConfigGlAttributeTest.java
+++ app/src/test/java/puscas/mobilertapp/configs/ConfigGlAttributeTest.java
@@ -3,10 +3,57 @@ package puscas.mobilertapp.configs;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import java.nio.ByteBuffer;
+
+import kotlin.UninitializedPropertyAccessException;
+
 /**
  * The test suite for {@link ConfigGlAttribute} class.
  */
 public final class ConfigGlAttributeTest {
+
+    /**
+     * Tests the default constructor of {@link ConfigGlAttribute}.
+     */
+    @Test
+    public void testDefaultConfigGlAttribute() {
+        Assertions.assertThatThrownBy(() -> ConfigGlAttribute.Builder.Companion.create().build())
+            .as("The ConfigGlAttribute#Builder#build shouldn't be possible with the default values")
+            .isInstanceOf(UninitializedPropertyAccessException.class);
+    }
+
+    /**
+     * Tests the building of {@link ConfigGlAttribute} with valid values.
+     */
+    @Test
+    public void testBuildConfigGlAttribute() {
+        final ConfigGlAttribute.Builder builder = ConfigGlAttribute.Builder.Companion.create();
+        final String attributeName = "test name";
+        final ByteBuffer byteBuffer = ByteBuffer.allocate(1);
+        final int attributeLocation = 123;
+        final int attributeComponentsInBuffer = 456;
+        builder.setAttributeName(attributeName);
+        builder.setBuffer(byteBuffer);
+        builder.setAttributeLocation(attributeLocation);
+        builder.setComponentsInBuffer(attributeComponentsInBuffer);
+        final ConfigGlAttribute configGlAttribute = builder.build();
+
+        Assertions.assertThat(configGlAttribute.getAttributeName())
+            .as("AttributeName not the expected value.")
+            .isEqualTo(attributeName);
+
+        Assertions.assertThat(configGlAttribute.getBuffer())
+            .as("Buffer not the expected value.")
+            .isSameAs(byteBuffer);
+
+        Assertions.assertThat(configGlAttribute.getAttributeLocation())
+            .as("AttributeLocation not the expected value.")
+            .isEqualTo(attributeLocation);
+
+        Assertions.assertThat(configGlAttribute.getComponentsInBuffer())
+            .as("ComponentsInBuffer not the expected value.")
+            .isEqualTo(attributeComponentsInBuffer);
+    }
 
     /**
      * Tests the {@link ConfigGlAttribute.Builder#toString()} method in the builder class of {@link Config}.
