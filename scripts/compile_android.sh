38a912c9e2e7f8bc83d6695db6414eb6992dd76d
TiagoMSSantos <tiago_s@outlook.pt>
2024-06-05 21:13:41 +0100

    chore(deps): update dependency gradle to v8.8

--- scripts/compile_android.sh
+++ scripts/compile_android.sh
@@ -107,10 +107,13 @@ build() {
     build_wrapper="./build-wrapper-linux-x86-64 --out-dir build_wrapper_output_directory";
   fi
 
-  echo 'Calling the Gradle assemble to compile code for Android.';
   echo 'Increasing ADB timeout to 10 minutes.';
   export ADB_INSTALL_TIMEOUT=60000;
+
+  echo 'Calling the Gradle assemble to compile code for Android.';
   sh gradlew --no-rebuild --stop --info --warning-mode fail --stacktrace;
+  echo "Setting Gradle Wrapper to a version that is compatible with Android API: '${android_api_version}'".;
+  sh gradlew wrapper -DandroidApiVersion="${android_api_version}";
   sh gradlew clean \
     build"${typeWithCapitalLetter}" \
     assemble"${typeWithCapitalLetter}" \
