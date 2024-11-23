68323b719bb84648bc251a9ddd9abbd1d3565e93
TiagoMSSantos <tiago_s@outlook.pt>
2024-11-23 16:08:02 +0000

    fix(deps): update dependency org.sonarsource.scanner.gradle:sonarqube-gradle-plugin to v6

--- scripts/run_tests_android.sh
+++ scripts/run_tests_android.sh
@@ -608,7 +608,7 @@ runInstrumentationTests() {
   if [ "${run_test}" = 'all' ]; then
     echo 'Running all tests';
     mkdir -p app/build/reports/jacoco/jacocoTestReport;
-    callCommandUntilSuccess 1 sh gradlew --offline ${gradle_command} -DtestType="${type}" \
+    callCommandUntilSuccess 1 sh gradlew ${gradle_command} -DtestType="${type}" \
       -DandroidApiVersion="${android_api_version}" \
       -Pandroid.testInstrumentationRunnerArguments.package='puscas' \
       -DabiFilters="[${cpu_architecture}]" \
@@ -616,14 +616,14 @@ runInstrumentationTests() {
   elif echo "${run_test}" | grep -q "rep_"; then
     run_test_without_prefix=${run_test#"rep_"};
     echo "Repeatable of test: ${run_test_without_prefix}";
-    callCommandUntilError sh gradlew --offline connectedAndroidTest -DtestType="${type}" \
+    callCommandUntilError sh gradlew connectedAndroidTest -DtestType="${type}" \
       -DandroidApiVersion="${android_api_version}" \
       -Pandroid.testInstrumentationRunnerArguments.class="${run_test_without_prefix}" \
       -DabiFilters="[${cpu_architecture}]" \
       --console plain --parallel --info --warning-mode all --stacktrace;
   else
     echo "Running test: ${run_test}";
-    sh gradlew --offline connectedAndroidTest -DtestType="${type}" \
+    sh gradlew connectedAndroidTest -DtestType="${type}" \
       -DandroidApiVersion="${android_api_version}" \
       -Pandroid.testInstrumentationRunnerArguments.class="${run_test}" \
       -DabiFilters="[${cpu_architecture}]" \
