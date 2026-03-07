514da6499787d91d8eb314aae81fcbb3d83a289a
TiagoMSSantos <tiago_s@outlook.pt>
2026-03-07 16:13:26 +0000

    fix(Utils): Allow to also print array of constant values
    Test plan:
    * Pipeline passes

--- app/MobileRT/Utils/Utils.hpp
+++ app/MobileRT/Utils/Utils.hpp
@@ -242,6 +242,23 @@ namespace MobileRT {
      * @param size The size of the array.
      */
     template<typename Size>
+    void addToStringStream(::std::ostringstream *const oss, const ::std::int32_t *const arrayValues, const Size size) {
+        for (Size i {}; i < size; ++i) {
+            const ::std::int32_t& parameterConst {arrayValues[i]};
+            addToStringStream(oss, parameterConst);
+            addToStringStream(oss, ", ");
+        }
+    }
+
+    /**
+     * Helper method which adds a parameter into the ostringstream.
+     *
+     * @tparam Size The size of the argument arrayValues.
+     * @param oss The ostringstream to add the parameters.
+     * @param arrayValues The parameter, which is an array to add in the ostringstream.
+     * @param size The size of the array.
+     */
+    template<typename Size>
     void addToStringStream(::std::ostringstream *const oss, ::std::int32_t *const arrayValues, const Size size) {
         for (Size i {}; i < size; ++i) {
             const ::std::int32_t& parameterConst {arrayValues[i]};
